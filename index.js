fbcf798a04ff49f21b2fdd3e1ad5b25f82b8a985
underfin <likui.underfin@gmail.com>
2025-05-20 12:05:21 +0800

    chore: update filename

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import oxc from 'oxc-transform';
 import fs from 'fs';
 
 const { code, declaration, errors } = oxc.transform(
-  'a.js',
-  fs.readFileSync('a.js', 'utf8'),
+  'input.js',
+  fs.readFileSync('input.js', 'utf8'),
   {"sourcemap":false,"jsx":{"development":true,"runtime":"automatic","importSource":"react","refresh":true},"lang":"tsx","typescript":{"onlyRemoveTypeImports":false,"removeClassFieldsWithoutInitializer":true},"assumptions":{"setPublicClassFields":true}}
 );
 
-fs.writeFileSync('b.js', code);
+fs.writeFileSync('output.js', code);
